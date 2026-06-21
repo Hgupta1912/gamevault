b27770a3d75d75e6e6460ad3ae964bc265aa35d4
Hgupta1912 <hans9g@gmail.com>
2026-06-21 17:25:48 -0400

    put in file structure and a tiny amount of code boilerplate

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+//import routers
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
---
+++ public/uploads/README.md
@@ -0,0 +1,1 @@
+Placeholder file so this directory will push to github!
